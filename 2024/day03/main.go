089cbf6bc2be5f9e7d76cb7ddd79e82c9964c605
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-12-21 14:20:17 -0800

    2024/03: part 1 - quick and dirty, but it works

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -1,5 +1,36 @@
-package day03
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+type Multiplication struct {
+	a, b int
+}
+
+func (m Multiplication) Product() int {
+	return m.a * m.b
+}
 
 func main() {
-	println("Hi")
+	b, _ := os.ReadFile("./day03/input.txt")
+	s := string(b)
+
+	r := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	ops := r.FindAllStringSubmatch(s, -1)
+	res := make([]Multiplication, 0, len(ops))
+	for _, op := range ops {
+		a, _ := strconv.Atoi(op[1])
+		b, _ := strconv.Atoi(op[2])
+		res = append(res, Multiplication{a, b})
+	}
+
+	result := 0
+	for _, m := range res {
+		result += m.Product()
+	}
+	fmt.Printf("Part 1: %d\n", result)
 }
